611e8600154c8e35e397b3f1716e8ad427b9bd59
josefl <jose.fl_@hotmail.com>
2022-04-27 23:52:40 -0500

    req 2. date properties for object (with test modified)

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -2,13 +2,13 @@ const User = require('../../app/models/User');
 
 describe("Unit test for User class", () => {
     test('Create an User object', () => {
-        const user = new User(1, "josefl19", "Jose Fujarte", "Bio", "dateCreated", "lastUpdate");
+        const user = new User(1, "josefl19", "Jose Fujarte", "Bio");
 
         expect(user.id).toBe(1);
         expect(user.username).toBe("josefl19");
         expect(user.name).toBe("Jose Fujarte");
         expect(user.bio).toBe("Bio");
-        expect(user.dateCreated).toBe("dateCreated");
-        expect(user.lastUpdate).toBe("lastUpdate");
+        expect(user.dateCreated).not.toBeUndefined();
+        expect(user.lastUpdate).not.toBeUndefined();
     });
 })
