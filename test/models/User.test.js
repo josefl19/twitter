9fc15a80d0452569b8f9b96efb7c0b52a439d8b8
josefl <jose.fl_@hotmail.com>
2022-04-27 23:36:13 -0500

    corrected syntax in test user

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -4,11 +4,11 @@ describe("Unit test for User class", () => {
     test('Create an User object', () => {
         const user = new User(1, "josefl19", "Jose Fujarte", "Bio", "dateCreated", "lastUpdate");
 
-        expected(user.id).toBe(1);
-        expected(user.username).toBe("josefl19");
-        expected(user.name).toBe("Jose Fujarte");
-        expected(user.bio).toBe("Bio");
-        expected(user.dateCreated).toBe("dateCreated");
-        expected(user.lastUpdate).toBe("lastUpdate");
+        expect(user.id).toBe(1);
+        expect(user.username).toBe("josefl19");
+        expect(user.name).toBe("Jose Fujarte");
+        expect(user.bio).toBe("Bio");
+        expect(user.dateCreated).toBe("dateCreated");
+        expect(user.lastUpdate).toBe("lastUpdate");
     });
 })
