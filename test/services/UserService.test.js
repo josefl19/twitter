dc46cabf68f5c3453f09c8844b410a9627dd89e8
josefl <jose.fl_@hotmail.com>
2022-04-29 00:48:01 -0500

    test corrected

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -2,7 +2,7 @@ const UserService = require('../../app/services/UserService');
 
 describe("Pruebas para la clase UserService", () => {
     test("Create a user with method static", () => {
-        const user = new UserService.create(1, "josefl", "Jose Fujarte");
+        const user = UserService.create(1, "josefl", "Jose Fujarte");
 
         expect(user.id).toBe(1);
         expect(user.username).toBe("josefl");
