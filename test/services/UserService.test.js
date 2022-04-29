22b6c089df4d08c5452c3a6803f8348bf1a7c0a7
josefl <jose.fl_@hotmail.com>
2022-04-29 00:46:21 -0500

    create service structure, req 1 UserService class and test

---
+++ test/services/UserService.test.js
@@ -0,0 +1,12 @@
+const UserService = require('../../app/services/UserService');
+
+describe("Pruebas para la clase UserService", () => {
+    test("Create a user with method static", () => {
+        const user = new UserService.create(1, "josefl", "Jose Fujarte");
+
+        expect(user.id).toBe(1);
+        expect(user.username).toBe("josefl");
+        expect(user.name).toBe("Jose Fujarte");
+        expect(user.bio).not.toBeUndefined();
+    });
+});
