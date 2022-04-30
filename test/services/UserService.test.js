4078f8053212a0f3cac62e41d8f597ad9d8c8bd0
josefl <jose.fl_@hotmail.com>
2022-04-30 13:25:33 -0500

    spr_2-req_2. static method getInfo added (with test)

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -1,12 +1,22 @@
 const UserService = require('../../app/services/UserService');
 
 describe("Pruebas para la clase UserService", () => {
-    test("Create a user with method static", () => {
+    test("Req 1) Create a user with method static", () => {
         const user = UserService.create(1, "josefl", "Jose Fujarte");
 
         expect(user.id).toBe(1);
         expect(user.username).toBe("josefl");
         expect(user.name).toBe("Jose Fujarte");
         expect(user.bio).not.toBeUndefined();
+    });
+
+    test("Req 2) Static method getInfo to return a list of values from an object", () => {
+        const user = UserService.create(1, "josefl", "Jose Fujarte");
+        const userInfoList = UserService.getInfo(user);
+
+        expect(userInfoList[0]).toBe(1);
+        expect(userInfoList[1]).toBe("josefl");
+        expect(userInfoList[2]).toBe("Jose Fujarte");
+        expect(userInfoList[3]).toBe("Sin bio");       
     });
 });
