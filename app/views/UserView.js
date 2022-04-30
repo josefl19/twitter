30518c5399ec90ca110dab0a62660da7cae895bf
josefl <jose.fl_@hotmail.com>
2022-04-30 16:20:55 -0500

    spr3_req2. return error object when try to create a user with invalid properties

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -2,6 +2,8 @@ class UserView {
     static createUser(payload) {
         if(payload === null) {
             return {error: 'payload no existe'};
+        } else if (Object.values(payload).some((p) => p === null)) {
+            return {error: 'necesitan tener un valor valido'};
         }
     }
 }
